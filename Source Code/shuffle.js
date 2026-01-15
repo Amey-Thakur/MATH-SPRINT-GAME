4b3fb25736be5e22c357712e13a2f4ab8ac4ca71
Amey Thakur <ameythakur20@gmail.com>
2026-01-15 01:32:59 -0500

    MATH-SPRINT-GAME

--- Source Code/shuffle.js
+++ Source Code/shuffle.js
@@ -1,3 +1,13 @@
+/**
+ * Project: Math Sprint Game
+ * File: shuffle.js
+ * Date: February 17, 2022
+ * Description: Utility function to shuffle array elements (Fisher-Yates algorithm).
+ * 
+ * Created by: Amey Thakur (https://github.com/Amey-Thakur) & Mega Satish (https://github.com/msatmod)
+ * Repository: https://github.com/Amey-Thakur/MATH-SPRINT-GAME
+ * License: MIT
+ */
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
